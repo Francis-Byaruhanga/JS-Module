cb0f025a97f2315c44e0d0bfd377ac3c9b33ff6d
Francis Byaruhanga <85689504+Francis-Byaruhanga@users.noreply.github.com>
2023-03-11 17:47:58 +0300

    Update bugs.js
    Increased salary amount.

--- bugs.js
+++ bugs.js
@@ -1,7 +1,8 @@
 var interestRate = 5.2;
-var salary = 2000;
+var salary = 3000;
 var months = 6; 
 var loan = 150;
 var compoundInterest = loan * interestRate / months;
 console.log('the salary variable is set to' + " " + salary);
 console.log('the compoundInterest variable is set to' + " " + compoundInterest);
+// Updated Salary
