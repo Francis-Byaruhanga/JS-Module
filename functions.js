70dfeda7626c9f732191e5ba6f3a93fa3f020a63
Francis <byaruhangafrancis23@gmail.com>
2023-03-13 12:31:31 +0300

    Added more lines of code to Functions.js file

--- functions.js
+++ functions.js
@@ -19,8 +19,31 @@
 // add2(2,4);
 console.log(add2(7,4));
 
-// Example 3
+// Function definition
+function say() {
+   console.log("How are you?");
+}
+say();
+
+function state() {
+   console.log("Come in");
+}
+state();
 
 
+function Add(a, b, c, d) {
+   return a + b + c + d
+}
+Add()
+console.log(Add(40, 67, 28, 80));
 
- 
+function add3(left, right) { // left & right are parameters here
+   let sum = left + right;  // left & right are arguments here
+   return sum;
+}
+
+console.log(add3(3, 6, 5)); // 5 is ignored, prints 9
+console.log(add3(3)); // second argument missing; prints NaN
+
+// Local and Global variables
+// Local variable - has a limited scope (inside a function in which it is defined)
